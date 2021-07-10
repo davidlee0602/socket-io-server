03782bde56b66ab852fc8edadec54934ec640156
David Lee <david@Davids-Mac-mini.fios-router.home>
2021-07-10 01:45:54 -0400

    updated user connect

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath  = path.join(__dirname, '/../public');
+const port = process.env.PORT || 4000;
 
 const dotenv = require('dotenv');
 const app = express();
-
-const port = process.env.PORT || 4000;
-
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket)=>{
+    console.log("A new user connected.");
+
+    // uses socket param passed
+    socket.on('disconnect', ()=>{
+        console.log("A user disconnected.");
+    })
+})
+
+
+
+server.listen(port, () => {
     console.log(`Communication Server started in port: ${port}`);
 
 })
